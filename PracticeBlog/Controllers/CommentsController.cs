0f9897b5e378d712d3d5f318480efe4f0e93cb0c
DE-corp <crazyHOPHOP@rambler.ru>
2023-10-31 17:50:29 +0300

    Добавил контроллеры

---
+++ PracticeBlog/Controllers/CommentsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticeBlog.Data.Models;
+using PracticeBlog.Data.Repositories;
+
+namespace PracticeBlog.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private readonly IRepository<Comment> _repo;
+
+        public CommentsController(IRepository<Comment> repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var comments = await _repo.GetAll();
+            return View(comments);
+        }
+        [HttpGet]
+        public IActionResult GetCommentById()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> GetCommentById(int id)
+        {
+            var comment = await _repo.Get(id);
+            return View(comment);
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(Comment newComment)
+        {
+            await _repo.Add(newComment);
+            return View(newComment);
+        }
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Comment comment)
+        {
+            await _repo.Delete(comment);
+            return View(comment);
+        }
+        [HttpGet]
+        public IActionResult Update()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(Comment comment)
+        {
+            await _repo.Update(comment);
+            return View(comment);
+        }
+    }
+}
---
+++ PracticeBlog/Controllers/TagsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticeBlog.Data.Models;
+using PracticeBlog.Data.Repositories;
+
+namespace PracticeBlog.Controllers
+{
+    public class TagsController : Controller
+    {
+        private readonly IRepository<Tag> _repo;
+
+        public TagsController(IRepository<Tag> repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var tags = await _repo.GetAll();
+            return View(tags);
+        }
+        [HttpGet]
+        public IActionResult GetTagById()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> GetTagById(int id)
+        {
+            var tag = await _repo.Get(id);
+            return View(tag);
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(Tag newTag)
+        {
+            await _repo.Add(newTag);
+            return View(newTag);
+        }
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Tag tag)
+        {
+            await _repo.Delete(tag);
+            return View(tag);
+        }
+        [HttpGet]
+        public IActionResult Update()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(Tag tag)
+        {
+            await _repo.Update(tag);
+            return View(tag);
+        }
+    }
+}
