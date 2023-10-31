988a2df4211ce36fff673f0d9b8c9872d32c6ad7
DE-corp <crazyHOPHOP@rambler.ru>
2023-10-31 17:06:35 +0300

    Использовал паттерн UnitOfWork

--- PracticeBlog/Program.cs
+++ PracticeBlog/Program.cs
@@ -1,35 +1,49 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using PracticeBlog.Data.Context;
+using PracticeBlog.Data.Models;
+using PracticeBlog.Data.Repositories;
+using PracticeBlog.Extentions;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-// Add DB
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<BlogContext>(option => option.UseSqlServer(connectionString), ServiceLifetime.Singleton);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        // Add DB
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services
+            .AddDbContext<BlogContext>(options => options.UseSqlServer(connectionString))
+            .AddUnitOfWork()
+                .AddCustomRepository<User, UserRepository>()
+                .AddCustomRepository<Article, ArticlesRepository>()
+                .AddCustomRepository<Comment, CommentRepository>()
+                .AddCustomRepository<Tag, TagRepository>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
