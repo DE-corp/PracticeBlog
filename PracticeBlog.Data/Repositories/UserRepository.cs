fc373848e97698ef4de1261eb690cdbd04496be3
DE-corp <crazyHOPHOP@rambler.ru>
2023-10-31 15:35:02 +0300

    Создал репозитории для бизнес моделей

---
+++ PracticeBlog.Data/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using PracticeBlog.Data.Context;
+using PracticeBlog.Data.Models;
+
+namespace PracticeBlog.Data.Repositories
+{
+    public class UserRepository : Repository<User>
+    {
+        public UserRepository(BlogContext db) : base(db) { }
+
+        public new async Task Add(User user)
+        {
+            user.Roles.Add(new Role { ID = 1, Name = "Admin" });
+            Set.Add(user);
+            await _db.SaveChangesAsync();
+        }
+        public User GetByLogin(string login)
+        {
+            return Set.FirstOrDefault(x => x.Login == login);
+        }
+    }
+}
